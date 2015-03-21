6393bd02445c9f7817cbe51faf26257ee75d194b
Amar Kumar Garg <amar.kumar@comprotechnologies.com>
2015-03-21 16:40:12 +0530

    Updated swagger UI

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/dist'));
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
